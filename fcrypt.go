062b54c38faf1b7087839a1e2ab56a6907fa2547
Nemuel Wainaina <bR0S3@protonmail.com>
2023-09-09 07:10:51 +0300

    Add comments

--- fcrypt.go
+++ fcrypt.go
@@ -7,36 +7,34 @@ import (
 )
 
 var (
-	enc, dec bool
-	file, dir string; recur bool
-	algo, key string; rand_key bool
+	help bool // display the help menu
+	enc, dec string // file/directory to encrypt/decrypt
+	recur bool // recursive option for directories
+	algo, key string // the algorithm and key to use
+	rand_key bool // option to generate and use a random key
 )
 
 func main() {
-	flag.BoolVar(&enc, "e", false, "Encrypt")
-	flag.BoolVar(&dec, "d", false, "Decrypt")
-
-	flag.StringVar(&file, "f", "", "File")
-	flag.StringVar(&dir, "D", "", "Directory")
-	flag.BoolVar(&recur, "r", false, "Recur")
-
-	flag.StringVar(&algo, "a", "", "Algorithm")
-	flag.StringVar(&key, "k", "", "Key")
-	flag.BoolVar(&rand_key, "rand-key", false, "Random key")
-
+	flag.BoolVar(&help, "h", false, "Print the help menu")
+	flag.StringVar(&enc, "e", "", "File or directory to encrypt")
+	flag.StringVar(&dec, "d", "", "File or directory to decrypt")
+	flag.BoolVar(&recur, "r", false, "Recursive option for a directory")
+	flag.StringVar(&algo, "a", "", "Algorithm to use")
+	flag.StringVar(&key, "k", "", "Encryption or decryption key")
+	flag.BoolVar(&rand_key, "rand-key", false, "Generate and use a random key")
 	flag.Parse()
 
-	if (enc && dec) || (!enc && !dec) {
-		print_help(os.Stderr)
-	}
-
-	fmt.Println(enc, dec, recur, algo, key, rand_key)
+	fmt.Println(enc)
 }
 
 func print_help(f *os.File) {
 	fmt.Fprintf(f, "Usage:\n")
 }
 
-func parse_flags() {
+func Encrypt(algorithm string, key string, content []byte) {
 
+}
+
+func EncryptFile(file string) {
+	
 }
